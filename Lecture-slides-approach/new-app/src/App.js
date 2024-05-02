46342a71e6e6f5b26c93b6bc52fe72794802c687
Lennox Mountain <lennoxmountain67@gmail.com>
2024-05-02 12:12:46 +0200

    added tailwindcss for styling

--- Lecture-slides-approach/new-app/src/App.js
+++ Lecture-slides-approach/new-app/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+//commented react import is just the same as the import up here. just different styles hooo😂😂😂
+// import * as React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center">
+        <h1>Hello React 👍🥳🥳🥳🥳</h1>
     </div>
   );
 }
