223be49bc6a92b448cedbe1e2e418d8bb18c3372
Lennox Mountain <lennoxmountain67@gmail.com>
2024-05-10 10:56:02 +0200

    installed react router dom

--- Road to React Approach/basic-project/src/App.jsx
+++ Road to React Approach/basic-project/src/App.jsx
@@ -2,9 +2,16 @@ import * as React from 'react'
 
 const name = "Lennox"
 function App() {
+    const welcome = {
+        title: "Welcome",
+        name: "User"
+    }
  return (
-  <h1 className="flex text-5xl text-blue-900 text-center justify-center align-middle">Hello {name} react + vite developer</h1>
-  
+  <div>
+    <h1 className="flex text-3xl text-blue-900 text-center justify-center align-middle"> {welcome.title} {welcome.name} </h1>
+    <label htmlFor="input">Name:</label>
+    <input className="border border-blue-600 focus: ring-cyan-800 ring-2 border-r-2 outline-0" type="text" id="input" />
+  </div>
  )
 }
 
