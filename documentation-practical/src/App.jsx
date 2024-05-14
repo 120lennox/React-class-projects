e55c1bf0b38cfcc4fe80c0f6bfb2f1c176b739d2
Lennox Mountain <lennoxmountain67@gmail.com>
2024-05-14 21:48:57 +0200

    discovering props

--- documentation-practical/src/App.jsx
+++ documentation-practical/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const user = "Lennox Mountain"
+  const home = {
+    names: "Lennox Mountain",
+    fav_food: "Nsima ya mbewa",
+    age: 2024 - 2003,
+    marital_status: "single",
+    theme: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItemns: 'center',
+      backgroundColor: 'grey',
+    }
+  }
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={home.theme}>
+      <h1> {user}'s profile </h1>
+      <ul>
+        <li>Favorite food: {home.fav_food} </li>
+        <li>Age: {home.age} </li>
+        <li>Marital status: {home.marital_status} </li>
+      </ul>
+    </div>
+  );
 }
 
 export default App
