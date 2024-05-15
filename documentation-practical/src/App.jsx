29348cf0b26d4d27be27f8d4e919999f552bff77
Lennox Mountain <lennoxmountain67@gmail.com>
2024-05-15 15:36:00 +0200

    added tailwindcss

--- documentation-practical/src/App.jsx
+++ documentation-practical/src/App.jsx
@@ -4,11 +4,16 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Home from "./components/Home"
 import About from "./components/About"
 import Signup from "./components/Signup"
+import Login from "./components/Login"
 
 function App() {
   return(
     <div>
-      <h1>Hello world</h1>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
     </div>
   )
 }
